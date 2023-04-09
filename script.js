975acf6c3318a1726b86b1ca1ef7f37e4f91bfb7
DinaraKnyazeva <Dinarka470@mail.ru>
2023-04-09 21:21:25 +0400

    comleted task

--- script.js
+++ script.js
@@ -2,15 +2,8 @@
 let userName = document.getElementById("name");
 //выбираю элемент с id="link" (инпут где указана ссылка на аватар)
 let userLink = document.getElementById("link");
-
 //выбираю элемент с id="comments" (инпут c комментарием)
 let userComments = document.getElementById("comments");
-
-// function capitalizeFully(str) {
-//   return str.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
-//     return a.toUpperCase();
-//   });
-// }
 
 //проверка и преобразование имени
 userName.addEventListener("blur", () => {
@@ -18,10 +11,59 @@ userName.addEventListener("blur", () => {
   elementName = elementName
     .toLowerCase()
     .replace(/\b\w/g, (l) => l.toUpperCase());
-  // elementName.value = capitalizeFully(elementName);
   userName.value = elementName;
 });
 
-let elementLink = userLink.value;
-let elementComments = userComments.value;
-console.log(elementName);
+//функция для проверки на спам
+function checkSpam(str) {
+  let newStr = str.replace(/viagra|xxx/gi, "***");
+  return newStr;
+}
+
+//выбираю элемент с классом "chat__comment" (контейнер для комментариев)
+let chatContainer = document.querySelector(".chat__comment");
+
+//обработка отправки формы
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault(); //отменяем стандартное поведение браузера
+
+  //создаю новый элемент "div"
+  let newComment = document.createElement("div");
+
+  //создаю элементы для нового комментария
+  let newAvatar = document.createElement("img");
+  let newName = document.createElement("p");
+  let newText = document.createElement("p");
+
+  //присваиваю классы элементам
+  newComment.classList.add("chat__result");
+  newAvatar.classList.add("chat__avatar");
+  newName.classList.add("chat__name");
+  newText.classList.add("chat__text");
+
+  //получаю значения из инпутов формы
+
+  let elementLink = userLink.value;
+  let elementComments = userComments.value;
+
+  //проверяю, что все поля заполнены
+  if (elementName !== "" && elementLink !== "" && elementComments !== "") {
+    //устанавливаю значения для элементов комментария
+    newAvatar.src = elementLink;
+    newName.textContent = elementName;
+    newText.textContent = checkSpam(elementComments);
+
+    //добавляю элементы в новый комментарий
+    newComment.appendChild(newAvatar);
+    newComment.appendChild(newName);
+    newComment.appendChild(newText);
+
+    //добавляю новый комментарий в контейнер
+    chatContainer.appendChild(newComment);
+
+    //очищаю значения инпутов
+    userName.value = "";
+    userLink.value = "";
+    userComments.value = "";
+  }
+});
